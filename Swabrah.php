8002c97922d9011d8aca4ec2c37665a4f1a4d8eb
Swabrah <swabrahkivumbi41@gmail.com>
2024-09-19 17:49:45 +0300

    Swabrah.php

---
+++ Swabrah.php
@@ -0,0 +1,13 @@
+<?php 
+ $Myname="Nambatya Swabrah";
+$DOB="18/08/2003";
+$Address="Lugoba,Kawempe-Kampala.";
+echo "Name: ".$Myname."\n";
+echo "Date of Birth: ".$DOB ."\n";
+echo "Home Address: ".$Address."\n";
+echo "You are welcome home dear ".$Myname;
+ $Current=2024;
+$Previous=2003;
+$Age=$Current -$Previous;
+echo  "and you are now ".$Age."years old.";
+?>
